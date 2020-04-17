8254b0dfe1d3aabbd69cce4a531e7295a66d8dd7
To Duc <20toduc01@gmail.com>
2020-04-17 20:21:38 +0700

    Homepage rework

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,54 @@
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
+// Design by Catalin V. (@hiskio https://twitter.com/hiskio)
+// https://dribbble.com/shots/3408986-Info-Card-Daily-UI-045
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
+const slideElements = ['.back__slide', '.card__slide', '.content__slide'];
+let inProgress = false;
 
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
+const goToSlide = (slideElements, index) => {
+  if (!inProgress) {
+    inProgress = true;
+
+    $('.active').addClass('exit');
+    $('.active').removeClass('active');
+    slideElements.forEach(elem => {
+      $(`${elem}:nth-child(${index})`).addClass('active');
+    });
+
+    const evenSlide = index % 2 === 0;
+    if (evenSlide)
+    $('.content__ping').addClass('content__ping--right');else
+
+    $('.content__ping').removeClass('content__ping--right');
+    $('.content__ping--noanimation').removeClass('content__ping--noanimation');
+
+    setTimeout(() => $('.exit').removeClass('exit'), 1000);
+    setTimeout(() => inProgress = false, 2000);
   }
+};
 
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
+$('.content__slide:nth-child(1)').on('click', () => goToSlide(slideElements, 2));
+$('.content__slide:nth-child(2)').on('click', () => goToSlide(slideElements, 1));
 
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
+setTimeout(() => goToSlide(slideElements, 2), 2000);
+setTimeout(() => goToSlide(slideElements, 1), 6000);
 
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
+// let amount = 0;
+// let slide = 0;
 
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
+// const progress = () => {
+//   amount++
+//   $('.active .progress').css('transform', `scaleX(${amount/400})`)
+//   if (amount >= 400){
+//     amount = 0;
+//     $('.active .progress').css('transform', `scaleX(${amount/400})`)
+//     slide = (slide + 1) % 2 ;
+//     goToSlide(slideElements, slide + 1);
+//     clearInterval(progressInterval);
+//     setTimeout(()=>{
+//       progressInterval = setInterval(progress,20);
+//       $('.back__slide:not(.active) .progress').css('transform', 'scaleX(0)')
+//     }, 2000);
+//   }
+// }
 
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
+// let progressInterval = setInterval(progress,20);
